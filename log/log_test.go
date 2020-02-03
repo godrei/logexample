e198ee2a074e4c88e4dcf854e7b48f1b55a21b71
Gödrei Krisztián <godreikrisztian@gmail.com>
2020-02-03 11:53:03 +0100

    log test

---
+++ log/log_test.go
@@ -0,0 +1,24 @@
+package log_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/godrei/logexample/log"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand(t *testing.T) {
+	var buff bytes.Buffer
+	log.SetOut(&buff)
+
+	in := "xcode build --project myproject.xcproj"
+	log.Command(in)
+
+	var out map[string]string
+	require.NoError(t, json.Unmarshal(buff.Bytes(), &out))
+	require.NotEqual(t, "", out["@time"])
+	require.Equal(t, "cmd", out["@type"])
+	require.Equal(t, in, out["msg"])
+}
