76ee9e8f89c709f352e63b9b868ebcf4ebcd7042
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-02-13 14:29:05 +0100

    more tests

--- log/log_test.go
+++ log/log_test.go
@@ -22,3 +22,17 @@ func TestCommand(t *testing.T) {
 	require.Equal(t, "cmd", out["@type"])
 	require.Equal(t, in, out["msg"])
 }
+
+func TestInfo(t *testing.T) {
+	var buff bytes.Buffer
+	log.SetOut(&buff)
+
+	in := "Info message"
+	log.Info(in)
+
+	var out map[string]string
+	require.NoError(t, json.Unmarshal(buff.Bytes(), &out))
+	require.NotEqual(t, "", out["@time"])
+	require.Equal(t, "inf", out["@type"])
+	require.Equal(t, in, out["msg"])
+}
